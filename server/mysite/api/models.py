24ac6592ed0e623c3cb5896470a2f838c3b45580
Vicki Bayang <victoriabayang0@gmail.com>
2024-10-05 21:05:53 -0500

    restructured front end to be forms and implimented useStates

--- server/mysite/api/models.py
+++ server/mysite/api/models.py
@@ -2,16 +2,33 @@ from django.db import models
 import datetime
 
 class Volunteer(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    age = models.IntegerField(default=0)
+    VOLUNTEER_GENDER = {
+        ('Male', 'male'), ('Female', 'female'), ('Non-binary/Other', 'other')
+    }
+    AGE = {
+        ("Under 15", ''),
+       ("15-17", '15-17'),
+        ("18-20", '18-20'),
+        ("21-30", '21-30'),
+        ("31-40", '31-40'),
+        ("41-50", '41-50'),
+        ("51-60", '51-60'),
+        ("61+", '61+')
+    }
+    
+    name = models.CharField(max_length=100)
+    gender = models.CharField(choice=VOLUNTEER_GENDER, default="Gender")
+    age = models.IntegerField(default=1)
+    address1 = models.CharField(max_length=100)
+    address1 = models.CharField(max_length=100)
+    
 
     def __str__(self):
         return self.first_name
     
 class Event(models.Model):
     EVENT_SKILLS = {
-        ('Not urgent', 'not_urgent'),
+        ('Not urgent', 'not_urgent'), #front end, variable name
         ('Urgent', 'urgent'),
         ('Very urgent', 'very_urgent'),
     }
